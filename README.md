96f6d3f69dbeee700ede5089e8a9417aef326b4a
wilhamsJW <wilhams.dev@gmail.com>
2022-01-24 11:17:59 -0300

    feat[home]: building-menu

--- README.md
+++ README.md
@@ -5,7 +5,6 @@
 1. `npx create-react-app client`
 
 [Comandos abaixo pego no site do Material UI](https://mui.com/pt/getting-started/installation/)
-
 Lembrete: Certifique-se de adicionar as fonts no html como link que o material ui pede na docs.
 
 2. `npm install @mui/material @mui/styled-engine-sc styled-components`
@@ -13,3 +12,5 @@ Lembrete: Certifique-se de adicionar as fonts no html como link que o material u
 3. `npm install @mui/material @emotion/react @emotion/styled` 
 
 4. `npm install @mui/icons-material`
+
+5. `npm install @material-ui/core`
--- client/src/Home.js
+++ client/src/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 
-import { makeStyles } from "@material-ui/core";
+import { makeStyles, AppBar, Box, Toolbar, Typography, Button, IconButton } from "@material-ui/core";
+import MenuIcon from '@mui/icons-material/Menu';
 
 // A partir do momento que passamos o parametro theme no makeStyles conseguimos acessar as propriedades css da const theme q está no App.js
 // pq isso acontece? Pq no <ThemeProvider /> q está no App.js ele recebe a variavel com as propriedades do tema e repassa essa var para
@@ -10,13 +11,37 @@ const useStyles = makeStyles((theme) => ({
     background: theme.palette.primary.main,
     height: "100vh",
   },
+  appBar: {
+    boxShadow: 'none'
+  },
+  grow: {
+    flexGrow: 1
+  }
 }));
 
 function Home() {
   // Aqui estamos instanciando a const usestyles para podermos usar ela abaixo
   const classes = useStyles();
 
-  return <div className={classes.root}>Olá Mundo</div>;
+  return ( 
+  <div className={classes.root}>
+      <AppBar color="inherit" className={classes.appBar}>
+        <Toolbar>
+          <IconButton
+            size="large"
+            edge="start"
+            color="inherit"
+            aria-label="menu"
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <div className={classes.grow} />
+          <Button color="inherit">FAZER LOGIN</Button>
+        </Toolbar>
+      </AppBar>
+  </div>
+  )
 }
 
 export default Home;
