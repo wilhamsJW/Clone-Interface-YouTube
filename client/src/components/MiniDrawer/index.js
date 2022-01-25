e17f5a940ce0e6f0a066a79a7a9cd88518945048
wilhamsJW <wilhams.dev@gmail.com>
2022-01-25 12:51:20 -0300

    feat[]: added-videos-array

--- client/src/components/MiniDrawer/index.js
+++ client/src/components/MiniDrawer/index.js
@@ -18,7 +18,7 @@ import ListItemText from "@mui/material/ListItemText";
 import InboxIcon from "@mui/icons-material/MoveToInbox";
 import MailIcon from "@mui/icons-material/Mail";
 
-import { makeStyles, Toolbar, Button, IconButton } from "@material-ui/core";
+import { makeStyles, Toolbar, Button, IconButton, Grid } from "@material-ui/core";
 import AccountCircleIcon from "@mui/icons-material/AccountCircle";
 import VideoCallIcon from "@mui/icons-material/VideoCall";
 import MoreVert from "@mui/icons-material/MoreVert";
@@ -35,8 +35,6 @@ import HistoryToggleOffIcon from '@mui/icons-material/HistoryToggleOff';
 import ThumbUpIcon from '@mui/icons-material/ThumbUp';
 import AddCircle from '@mui/icons-material/AddCircle';
 import ListSubheader from '@mui/material/ListSubheader';
-import { typography } from "@mui/system";
-
 const drawerWidth = 240;
 
 const openedMixin = (theme) => ({
@@ -148,6 +146,88 @@ function MiniDrawer() {
   // Aqui estamos instanciando a const usestyles para podermos usar ela abaixo
   const classes = useStyles();
   // const theme = useTheme();
+
+  const videos = [
+    {
+      id: 1,
+      title:
+        'FEED DO USUÁRIO | Criando uma Rede Social com React.js e .NET Core #29',
+      channel: 'Lucas Nhimi',
+      views: '11 mi de visualizações',
+      date: 'há 1 semana',
+      avatar: '/images/avatar.jpeg',
+      thumb: '/images/thumb1.png',
+    },
+    {
+      id: 2,
+      title:
+        'COMO MELHORAR SEU CODIGO JAVASCRIPT (ESLINT + PRETTIER + EDITORCONFIG) | Dicas e Truques #02',
+      channel: 'Lucas Nhimi',
+      views: '957 mil visualizações',
+      date: 'há 1 semana',
+      avatar: '/images/avatar.jpeg',
+      thumb: '/images/thumb2.png',
+    },
+    {
+      id: 3,
+      title:
+        'CONTEXT API NO EDITOR DE POST | Criando uma Rede Social com React.js e .NET Core #27',
+      channel: 'Lucas Nhimi',
+      views: '106 mil visualizações',
+      date: 'há 1 semana',
+      avatar: '/images/avatar.jpeg',
+      thumb: '/images/thumb3.png',
+    },
+    {
+      id: 4,
+      title:
+        'CONTEXT API NO EDITOR DE POST | Criando uma Rede Social com React.js e .NET Core #27',
+      channel: 'Lucas Nhimi',
+      views: '5,6 mi de visualizações',
+      date: 'há 1 semana',
+      avatar: '/images/avatar.jpeg',
+      thumb: '/images/thumb4.png',
+    },
+    {
+      id: 5,
+      title:
+        'EDITOR DE POST COM MARKDOWN 2 | Criando uma Rede Social com React.js e .NET Core #26',
+      channel: 'Lucas Nhimi',
+      views: '2,2 mi de visualizações',
+      date: 'há 1 semana',
+      avatar: '/images/avatar.jpeg',
+      thumb: '/images/thumb5.png',
+    },
+    {
+      id: 6,
+      title: 'COMO MIGRAR PARA REACT HOOKS | Dicas e Truques #01',
+      channel: 'Lucas Nhimi',
+      views: '233 mil visualizações',
+      date: 'há 1 semana',
+      avatar: '/images/avatar.jpeg',
+      thumb: '/images/thumb6.png',
+    },
+    {
+      id: 7,
+      title:
+        'PRÉ-REQUISITOS | Criando uma Rede Social com React.js e .NET Core #01',
+      channel: 'Lucas Nhimi',
+      views: '118 mil visualizações',
+      date: 'há 1 semana',
+      avatar: '/images/avatar.jpeg',
+      thumb: '/images/thumb7.png',
+    },
+    {
+      id: 8,
+      title:
+        'GIT E GITHUB | Criando uma Rede Social com React.js e .NET Core #04',
+      channel: 'Lucas Nhimi',
+      views: '1,9 mi de visualizações',
+      date: 'há 1 semana',
+      avatar: '/images/avatar.jpeg',
+      thumb: '/images/thumb8.png',
+    },
+  ];
 
   return (
     <Box sx={{ display: "flex" }}>
@@ -438,7 +518,11 @@ function MiniDrawer() {
            Recomendado       
         </Typography>
 
-                    
+        <Grid container>
+          <Grid item lg={4}>x</Grid>
+          <Grid item lg={4}>x</Grid>
+          <Grid item lg={4}>x</Grid>
+        </Grid>            
 
       </Box>
       </Box> 
